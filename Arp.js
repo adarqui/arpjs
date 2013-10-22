463046ed4906bab4168ae764b35b60d626ef977e
Andrew Darqui <andrew.darqui@gmail.com>
2013-10-21 23:53:35 -0400

    arp

--- Arp.js
+++ Arp.js
@@ -24,8 +24,7 @@ var _Arp = function(opts) {
 
 
 	Arp.Show = function() {
-		var argv = [];
-		var cb;
+		var argv = [], cb, syntax;
 		if(arguments.length != 0 && typeof arguments[0] == 'object') {
 			Arp.GenericOptions(argv,arguments[0]);
 			cb = arguments[1];
@@ -37,12 +36,12 @@ var _Arp = function(opts) {
 			throw new Error('Arp.Show: ERROR => Provide a callback.');
 			return;
 		}
-		var syntax = Private.bin + ' ' + argv.join(' ');
+		syntax = Private.bin + ' ' + argv.join(' ');
 		Private.exec(syntax, function(err,stdout,stderr) {
-			var js = { };
+			var js = { }, lines;
 			if(!err) {
 				js.entries = [];
-				var lines = stdout.split('\n');
+				lines = stdout.split('\n');
 				lines.splice(0,1);
 				for(var v in lines) {
 					var line = lines[v];
@@ -67,7 +66,54 @@ var _Arp = function(opts) {
 		});
 	}
 
-	Arp.Set = function() {
+	Arp.Set = function(opts, cb) {
+		var argv = [], syntax;
+
+		var set_throw_error = function(s) {
+			throw new Error("Arp.Set: ERROR => " + s);
+			return;
+		}
+
+		if(!opts || !cb) {
+			return set_throw_error("Incorrect arguments");
+		}
+
+		if(!opts.address || (!opts.interface && !opts.hwaddr)) {
+			return set_throw_error("Provide an address & interface");
+		}
+
+//		argv.push(Private.bin).push('-Ds').push(opts.address).push(opts.interface);
+		/*
+		 * Can specify interface or hwaddr, not both
+		 */
+		if(opts.interface && opts.hwaddr) {
+			return set_throw_error("Specify one or the other: hwaddr or interface");
+		}
+		else if(opts.hwaddr) {
+			argv = argv.concat(Private.bin).concat('-s').concat(opts.address).concat(opts.hwaddr);
+		}
+		else if(opts.interface) {
+			argv = argv.concat(Private.bin).concat('-Ds').concat(opts.address).concat(opts.interface);
+		}
+		if(opts.netmask) {
+			argv = argv.concat('netmask').concat(opts.netmask);
+		}
+		var syntax = argv.join(' ');
+		console.log("syntax", syntax);
+		Cproc.exec(syntax,function(err,stdout,stderr) {
+			console.log(syntax,"err,stdout,stderr",err,stdout,stderr);
+			var ret, channel
+			if(err || stderr.length > 0) {
+				ret = false;
+				channel = stdout;
+			} else {
+				ret = true;
+				channel = stderr;
+			}
+			cb(ret,channel);
+		});
+
+
 	}
 
 	Arp.Del = function() {
