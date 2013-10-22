50ddfc5a2d78f30e24c40ddc8a4c7c093c4f0172
Andrew Darqui <andrew.darqui@gmail.com>
2013-10-21 23:53:39 -0400

    test

--- test.js
+++ test.js
@@ -11,4 +11,31 @@ Arp.Show(function(err,js) {
 Arp.Show({ numeric : true }, function(err,js) {
 	console.log("err",err,"js",js);
 });
-//Arp.Show();
+
+Arp.Set({
+	address : "192.168.1.7",
+	interface : "eth0"
+}, function(err,data) {
+	console.log(err,data);
+});
+
+Arp.Set({
+	address : "192.168.1.8",
+	interface : "wlan0"
+}, function(err,data) {
+	console.log(err,data);
+});
+
+Arp.Set({
+	address : "192.168.1.12",
+	hwaddr : "ca:dd:b4:44:41:a5",
+}, function(err,data) {
+	console.log(err,data);
+});
+
+Arp.Set({
+	address : "192.168.1.14",
+	interface : "vlan0",
+}, function(err,data) {
+	console.log(err,data);
+});
